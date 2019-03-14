e31f3e926e2e2929e65234270383db6be8fd23a6
Alex Machin <AlexMachin1997@gmail.com>
2019-03-14 14:23:19 +0000

    Settings page development is complete (#2)

--- src/screens/settings.js
+++ src/screens/settings.js
@@ -1,14 +1,62 @@
+// React dependencies
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, AsyncStorage} from 'react-native';
+
+/* 
+Util classes:
+- To access util classes use the exported variable.
+- Since the utils are objects you will need to access the properties like flex.justifyContentCenter or background.blue
+*/
+import {flex} from '../styles/flex-utils';
+import {fonts, textColour} from '../styles/text-utils';
+import {spacing} from '../styles/spacing-utils';
+import {background} from '../styles/background-utils';
+import {images} from '../styles/image-utils';
+
+import { Button, Divider} from 'react-native-paper';
 
 class Settings extends Component {
   render() {
     return (
+      <View style={[flex.enableFlex, flex.column, spacing.ContainerSpacing, flex.justifyContentSpaceEvenly]}>
+
       <View>
-        <Text>
-            Settings Component
-        </Text>
+        <Text style={[fonts.title1]}>Core application settings</Text>
       </View>
+
+      <Divider />
+
+      <View style={[spacing.smallTop]}>
+        <Text style={[fonts.title3]}>Account deletion</Text>
+        <Text style={[fonts.callout]}>Permanently delete your account and all of it's content</Text>
+          <Button mode="contained"  color="#FF0000" compact={true} onPress={()=> {}}>
+           Delete account
+          </Button>
+      </View>     
+
+      <Divider />
+
+      <View style={[spacing.smallTop]}>
+        <Text style={[fonts.title3]}>Delete contents</Text>
+        <Text style={[fonts.callout]}> Permanently delete all your application contents </Text>
+        <Button mode="contained"  color="#FF0000" compact={true} onPress={()=> {}}>
+           Delete data
+        </Button>
+      </View>
+
+      <Divider />
+
+      <View style={[spacing.smallTop]}>
+        <Text style={[fonts.title3]}>Delete contents</Text>
+        <Text style={[fonts.callout]}>Permanently delete all your application contents </Text>
+        <Button mode="contained"  color="#FF0000" compact={true} onPress={()=> {this.props.navigation.navigate('guestStack')}}>
+           Logout
+        </Button>
+      </View>     
+
+      <Divider />
+
+    </View>
     );
   }
 }
